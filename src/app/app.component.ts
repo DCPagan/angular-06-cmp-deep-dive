7b7796ae0bb01656a9a4b4ea5ec3ca1b665de4a9
D.C. Pagan <dcpagan@posteo.net>
2026-03-10 21:29:19 -0400

    Create dashboard-item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,15 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   imports: [
-    HeaderComponent, ServerStatusComponent, TicketsComponent, TrafficComponent
-  ],
+    DashboardItemComponent, HeaderComponent, ServerStatusComponent,
+    TicketsComponent, TrafficComponent,
+  ]
 })
-export class AppComponent {
-}
+export class AppComponent {}
---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, input, Input } from '@angular/core';
+
+interface Image {
+  src: string;
+  alt: string;
+}
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+  readonly image = input.required<Image>();
+  readonly title = input.required<string>();
+}
