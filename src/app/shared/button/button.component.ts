66d7c3896708e90fb261f77862b20a61292f10aa
D.C. Pagan <dcpagan@posteo.net>
2026-03-12 01:23:53 -0400

    Add form control component

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,11 +1,10 @@
 import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'a[appButton], button[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
-export class ButtonComponent {
-}
+export class ButtonComponent {}
