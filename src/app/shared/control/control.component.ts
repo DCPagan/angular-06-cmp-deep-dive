5ec3c69b2c91bac62fff769d65b2d6c02bc7cda4
D.C. Pagan <dcpagan@posteo.net>
2026-03-12 03:49:29 -0400

    Add host event listener

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -8,10 +8,17 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'
   readonly name = input<string>();
   readonly label = input.required<string>();
+
+  // @HostListener('click')
+  onClick(): void {
+    console.log('Clicked!')
+  }
 }
